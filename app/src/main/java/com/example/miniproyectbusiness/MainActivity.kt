8f80ae94ed89c1e2905e3ed6d94290ae4ec6b754
magnitopic <magnitopic@gmail.com>
2023-01-23 12:13:01 +0100

    added intents to app

--- app/src/main/java/com/example/miniproyectbusiness/MainActivity.kt
+++ app/src/main/java/com/example/miniproyectbusiness/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.example.miniproyectbusiness
 
+import android.content.Intent
 import android.graphics.Typeface
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.TextView
+import android.widget.Toast
+import androidx.core.net.toUri
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +16,53 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // cambio de fonts
-        val mainTitle : TextView = findViewById<TextView>(R.id.mainTitle)
-        val face : Typeface = Typeface.createFromAsset(assets, "fonts/JKAbode-Demo.ttf")
+        val mainTitle: TextView = findViewById<TextView>(R.id.mainTitle)
+        val face: Typeface = Typeface.createFromAsset(assets, "fonts/JKAbode-Demo.ttf")
         mainTitle.typeface = face
+    }
+
+    fun handler(vista: View) {
+        when (vista.id) {
+            R.id.web -> openWebPage("https://magnitopic.com")
+            R.id.email -> composeEmail(arrayOf("info@centronelson.org"))
+            R.id.direc -> showMap("geo: 40.4, -3.7".toUri())
+            R.id.telf -> dialPhoneNumber("646265852")
+        }
+    }
+
+    fun openWebPage(url: String) {
+        val webpage: Uri = Uri.parse(url)
+        val intent = Intent(Intent.ACTION_VIEW, webpage)
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun composeEmail(addresses: Array<String>) {
+        val intent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:") // only email apps should handle this
+            putExtra(Intent.EXTRA_EMAIL, addresses)
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun showMap(geoLocation: Uri) {
+        val intent = Intent(Intent.ACTION_VIEW).apply {
+            data = geoLocation
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
+    }
+
+    fun dialPhoneNumber(phoneNumber: String) {
+        val intent = Intent(Intent.ACTION_DIAL).apply {
+            data = Uri.parse("tel:$phoneNumber")
+        }
+        if (intent.resolveActivity(packageManager) != null) {
+            startActivity(intent)
+        }
     }
 }
